0391b318526c3c3af337d1cc48705324f4827fb0
Josh Seipel <joshseipel@gmail.com>
2017-02-02 16:15:30 -0500

    update game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,70 @@
 console.log("JS file is connected to HTML! Woo!")
+
+/* This is the first updated main.js file from Josh.*/
+
+var cardOne = 'king';
+var cardTwo = 'king';
+var cardThree = 'queen';
+var cardFour = 'queen';
+
+//Create var to stor gameBoard.//
+var gameBoard = document.getElementById('game-board');
+
+
+
+
+
+
+//Create a function that creates the cards for the game.//
+function createCards() {
+	for (var i=0; i < 4; i++) {		
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	gameBoard.appendChild(newCard);
+  }
+}
+//Calling the createCards function to show your cards.//
+createCards();
+
+
+
+//How do I write the code below without being redundant? or DRY? 
+
+/*
+if ( cardTwo === cardFour ) {
+	alert("Sorry try again!");
+} else if ( cardOne === cardThree ) {
+	alert("Sorry try again!");
+} else if ( cardTwo === cardThree ) {
+	alert("Sorry try again");
+} else if ( cardTwo === cardOne ) {
+	alert("You found a match!");
+} else if ( cardOne === cardFour ) {
+	alert("Sorry try again");
+} else if ( cardThree === cardFour ) {
+	alert("You found a match!");
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ starter-code/practice.js
@@ -0,0 +1,21 @@
+var fizzbuzz = function(num) {
+	for ( var i = 1; i <= num; i += 1 ) {
+		if (i%3 === 0 && i%5 === 0) {
+			result = "fizzbuzz";
+		} else if (i%3 === 0) {
+			result = "fizz";
+		} else if (i%5 === 0) {
+			result = "buzz";
+		} else {
+			result = i
+		}
+		console.log(result);
+	}
+};
+
+fizzbuzz(15);
+
+console.log('Hello');
+
+alert('hello');
+
