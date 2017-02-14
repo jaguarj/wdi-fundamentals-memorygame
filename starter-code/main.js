c8e9156b0ebb10183f33e2e74b9d91f693a6ebd2
Josh Seipel <joshseipel@gmail.com>
2017-02-14 12:17:44 -0500

    Updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,64 +1,77 @@
 console.log("JS file is connected to HTML! Woo!")
 
 /* This is the first updated main.js file from Josh.*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = 'king';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'queen';
-
-//Create var to stor gameBoard.//
 var gameBoard = document.getElementById('game-board');
 
 
+//This function creates a game board with a specified number of cards based off the cards array.//
+var createBoard = function(){
+		for (var i = 0; i < cards.length; i++){
+
+		//Creates a card element 'div' to be the card or cards depending on the length of the cards array.//	
+		var newCard = document.createElement('div');
+
+		//Sets the class 'card' to each div element.//	
+		newCard.classList.add('card');
+
+		//This will set the card's 'data-card' to be the element of the array.//
+		newCard.setAttribute('data-card', cards[i]);
+
+		//Runs the isTwoCards function when the card is clicked.//
+		newCard.addEventListener('click', isTwoCards);
+		//Appends card to the game-board.//
+		gameBoard.appendChild(newCard);
+			
+
+	}
+	reset();
+};
 
 
 
+function isTwoCards(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+		//console.log(this.getAttribute('data-card'));//
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = "<img src='images/king.png' alt='King' />";
 
-//Create a function that creates the cards for the game.//
-function createCards() {
-	for (var i=0; i < 4; i++) {		
-	var newCard = document.createElement('div');
-	newCard.className = 'card';
-	gameBoard.appendChild(newCard);
-  }
+		} else {
+			this.innerHTML = "<img src='images/queen.jpg' alt='Queen'/>";
+		};
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+
+	}
+
+};
+
+
+function isMatch(cards){
+	if (cards[0] === cards[1]) {
+		alert('You have a match!');
+	} else {		
+		
+		reset();	
+	} 
+	
+};
+
+
+function reset() {
+	for (var i = 0; i <cards.length; i++){
+		if(document.getElementsByClassName('card')[i]) {
+			document.getElementsByClassName('card')[i].innerHTML = "";
+		}
+	}
 }
-//Calling the createCards function to show your cards.//
-createCards();
 
 
-
-//How do I write the code below without being redundant? or DRY? 
-
-/*
-if ( cardTwo === cardFour ) {
-	alert("Sorry try again!");
-} else if ( cardOne === cardThree ) {
-	alert("Sorry try again!");
-} else if ( cardTwo === cardThree ) {
-	alert("Sorry try again");
-} else if ( cardTwo === cardOne ) {
-	alert("You found a match!");
-} else if ( cardOne === cardFour ) {
-	alert("Sorry try again");
-} else if ( cardThree === cardFour ) {
-	alert("You found a match!");
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
+createBoard();
 
 
 
