926797ce77c503820f104828a189f1cb59390ccd
Josh Seipel <joshseipel@gmail.com>
2017-02-18 15:59:01 -0500

    Updated HTML, CSS, Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,24 @@ console.log("JS file is connected to HTML! Woo!")
 /* This is the first updated main.js file from Josh.*/
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
 var gameBoard = document.getElementById('game-board');
 
+
+var sayHello = function(x){ 
+  var x = prompt("Hello stranger, what is your name? ");    
+  var name = x;  
+	  if (x === '' || x === null || x === undefined){
+	    alert("Really...no input? You can do better! Let's try this again!");    
+	  } 	  
+  var y = prompt("Would you like to play my game, " + name + '?'); 	  
+	  if ( y === "no" || y === "No" || y === '' || y === null ){	    
+	    alert("See ya round sucka!");
+	    return;	    
+	  } if ( y === "yes" || y === "Yes" || y === "sure" || y === "ok"){	    	    
+	    alert("Your Memory Game has started!! Hahahaha...good luck!!");   	    	  
+	    createBoard();
+	  }
+}; 
 
 //This function creates a game board with a specified number of cards based off the cards array.//
 var createBoard = function(){
@@ -68,11 +83,11 @@ function reset() {
 			document.getElementsByClassName('card')[i].innerHTML = "";
 		}
 	}
-}
+};
 
 
-createBoard();
 
+	
 
 
 
